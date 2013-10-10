475397f5c8554985fc841f28430cefa9828f3fe1
omar morales <omarmorales865@gmail.com>
2013-10-10 00:53:01 -0400

    Agrege la vista home

--- turismo/turismo/urls.py
+++ turismo/turismo/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'turismo.views.home', name='home'),
+    url(r'^$', 'app.views.home', name='home'),
     # url(r'^turismo/', include('turismo.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
